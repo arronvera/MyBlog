8da4984128a90ce3861e3501d1c2d3116296f330
arronvera <495167750@qq.com>
2017-03-10 10:04:45 +0800

    增加加载动画

--- app/src/main/java/code/vera/myblog/SplashActivity.java
+++ app/src/main/java/code/vera/myblog/SplashActivity.java
@@ -1,26 +1,65 @@
 package code.vera.myblog;
 
 import android.content.Intent;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Handler;
 import android.text.TextUtils;
 
+import com.hanks.htextview.HTextView;
+import com.hanks.htextview.HTextViewType;
+
+import butterknife.BindView;
+import code.vera.myblog.model.base.VoidModel;
+import code.vera.myblog.presenter.PresenterActivity;
+import code.vera.myblog.view.base.VoidView;
 import ww.com.core.Debug;
 
 /**
  * 欢迎界面
  */
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends PresenterActivity<VoidView,VoidModel> {
+    @BindView(R.id.logo_text)
+     HTextView textView;
+    private Handler handler;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-        isLogin();
-        Intent intent=new Intent(this,LoginActivity.class);
+    protected int getLayoutResId() {
+        return R.layout.activity_splash;
+    }
+
+    @Override
+    protected void onAttach() {
+        super.onAttach();
+        textView.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+        textView.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                textView.setAnimateType(HTextViewType.TYPER);
+                textView.animateText("用圈艺");
+            }
+        }, 100);
+        textView.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                textView.setAnimateType(HTextViewType.ANVIL);
+                textView.animateText("用圈艺,分享属于你的艺术");
+            }
+        }, 1000);
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                isLogin();
+            }
+        }, 2600);
+
+
 
     }
 
+    /**
+     * 是否登陆
+     */
     private void isLogin() {
         String token=AccessTokenKeeper.readAccessToken(getApplicationContext()).getToken();
         if (!TextUtils.isEmpty(token)){
