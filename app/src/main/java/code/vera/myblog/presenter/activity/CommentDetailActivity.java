e1d1ecdd7b77c5fa1aa72c8899e894f549d18549
arronvera <495167750@qq.com>
2017-03-10 18:00:30 +0800

    修改

--- app/src/main/java/code/vera/myblog/presenter/activity/CommentDetailActivity.java
+++ app/src/main/java/code/vera/myblog/presenter/activity/CommentDetailActivity.java
@@ -3,6 +3,8 @@ package code.vera.myblog.presenter.activity;
 import android.content.Intent;
 import android.view.View;
 
+import com.yalantis.contextmenu.lib.ContextMenuDialogFragment;
+
 import butterknife.OnClick;
 import code.vera.myblog.R;
 import code.vera.myblog.bean.home.RetweetedStatusBean;
@@ -24,6 +26,7 @@ public class CommentDetailActivity extends PresenterActivity<CommentDetailView, 
 
     public static long id;
     private StatusesBean statusesBean;
+    private ContextMenuDialogFragment mMenuDialogFragment;
     @Override
     protected int getLayoutResId() {
         return R.layout.activity_comment_detail;
@@ -32,6 +35,7 @@ public class CommentDetailActivity extends PresenterActivity<CommentDetailView, 
     @Override
     protected void onAttach() {
         super.onAttach();
+//        initMenuFragment();
         Intent intent = getIntent();
         //
         StatusesBean statusesBean= (StatusesBean) intent.getSerializableExtra("status");
@@ -52,6 +56,51 @@ public class CommentDetailActivity extends PresenterActivity<CommentDetailView, 
         view.setOnItemTopicListener(this);
         view.setOnItemLinkListener(this);
     }
+
+//    private void initMenuFragment() {
+//        MenuParams menuParams = new MenuParams();
+//        menuParams.setActionBarSize((int) getResources().getDimension(R.dimen.tool_bar_height));
+//        menuParams.setMenuObjects(getMenuObjects());
+//        menuParams.setClosableOutside(false);
+//        mMenuDialogFragment = ContextMenuDialogFragment.newInstance(menuParams);
+//        mMenuDialogFragment.setItemClickListener(this);
+//        mMenuDialogFragment.setItemLongClickListener(this);
+//    }
+//
+//    private List<MenuObject> getMenuObjects() {
+//
+//        List<MenuObject> menuObjects = new ArrayList<>();
+//
+//        MenuObject close = new MenuObject();
+//        close.setResource(R.drawable.icn_close);
+//
+//        MenuObject send = new MenuObject("Send message");
+//        send.setResource(R.drawable.icn_1);
+//
+//        MenuObject like = new MenuObject("Like profile");
+//        Bitmap b = BitmapFactory.decodeResource(getResources(), R.drawable.icn_2);
+//        like.setBitmap(b);
+//
+//        MenuObject addFr = new MenuObject("Add to friends");
+//        BitmapDrawable bd = new BitmapDrawable(getResources(),
+//                BitmapFactory.decodeResource(getResources(), R.drawable.icn_3));
+//        addFr.setDrawable(bd);
+//
+//        MenuObject addFav = new MenuObject("Add to favorites");
+//        addFav.setResource(R.drawable.icn_4);
+//
+//        MenuObject block = new MenuObject("Block user");
+//        block.setResource(R.drawable.icn_5);
+//
+//        menuObjects.add(close);
+//        menuObjects.add(send);
+//        menuObjects.add(like);
+//        menuObjects.add(addFr);
+//        menuObjects.add(addFav);
+//        menuObjects.add(block);
+//        return menuObjects;
+//    }
+
     @OnClick({R.id.tv_item_comment,R.id.tv_item_repost,R.id.rl_item_like})
     public void doClick(View v){
         switch (v.getId()){
