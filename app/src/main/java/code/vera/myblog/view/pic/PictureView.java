218a5787b78bfa440b19d9e730787594a982ee02
arronvera <495167750@qq.com>
2017-04-19 10:32:31 +0800

    新增gif图片显示

--- app/src/main/java/code/vera/myblog/view/pic/PictureView.java
+++ app/src/main/java/code/vera/myblog/view/pic/PictureView.java
@@ -1,10 +1,18 @@
 package code.vera.myblog.view.pic;
 
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.NonNull;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.gifdecoder.GifDecoder;
+import com.bumptech.glide.load.resource.gif.GifDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.assist.FailReason;
 import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
@@ -14,6 +22,9 @@ import code.vera.myblog.R;
 import code.vera.myblog.view.base.BaseView;
 import uk.co.senab.photoview.PhotoView;
 import uk.co.senab.photoview.PhotoViewAttacher;
+import ww.com.core.Debug;
+
+import static android.os.Looper.getMainLooper;
 
 /**
  * Created by vera on 2017/2/20 0020.
@@ -24,13 +35,29 @@ public class PictureView extends BaseView {
      PhotoView photoView;//图片
     @BindView(R.id.iv_progress)
     ImageView ivProgress;//进度条
+
     private PhotoViewAttacher photoViewAttacher;
     private Bitmap loadBitmap;//加载图片
+    private Context context;
+    private int duration=0;
+    private Handler handler;
+    private static final int WHAT_GIF_FINISH=0;
 
     @Override
     public void onAttachView(@NonNull View view) {
         super.onAttachView(view);
         photoViewAttacher=new PhotoViewAttacher(photoView,true);
+        context=view.getContext();
+        handler = new Handler(getMainLooper()) {
+            @Override
+            public void handleMessage(Message msg) {
+                switch (msg.what) {
+                    case WHAT_GIF_FINISH:
+                        //动画结束
+                        break;
+                }
+            }
+        };
     }
 
     /**
@@ -38,37 +65,58 @@ public class PictureView extends BaseView {
      * @param url
      */
     public void showPic(String url){
+        Debug.d("url="+url);
         if (url.endsWith(".gif")){
+            Glide.with(context).load(url).asGif().listener(new RequestListener<String, GifDrawable>() {
+                @Override
+                public boolean onException(Exception e, String model, Target<GifDrawable> target, boolean isFirstResource) {
+                    return false;
+                }
 
-        }
-        ImageLoader.getInstance().displayImage(url, photoView, new ImageLoadingListener() {
-            @Override
-            public void onLoadingStarted(String s, View view) {
-                //开始加载
-                ivProgress.setVisibility(View.VISIBLE);
-                ivProgress.setImageResource(R.drawable.loading_anim);
-            }
+                @Override
+                public boolean onResourceReady(GifDrawable resource, String model, Target<GifDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+                    //计算动画时长
+                    GifDrawable drawable = (GifDrawable) resource;
+                    GifDecoder decoder = drawable.getDecoder();
+                    for (int i = 0; i < drawable.getFrameCount(); i++) {
+                        duration += decoder.getDelay(i);
+                    }
+                    //发送延时消息，通知动画结束
+                    handler.sendEmptyMessageDelayed(WHAT_GIF_FINISH, duration);
+                    return false;
+                }
+            }).into(photoView);
+        }else {
+            ImageLoader.getInstance().displayImage(url, photoView, new ImageLoadingListener() {
+                @Override
+                public void onLoadingStarted(String s, View view) {
+                    //开始加载
+                    ivProgress.setVisibility(View.VISIBLE);
+                    ivProgress.setImageResource(R.drawable.loading_anim);
+                }
 
-            @Override
-            public void onLoadingFailed(String s, View view, FailReason failReason) {
-                ivProgress.setVisibility(View.GONE);
+                @Override
+                public void onLoadingFailed(String s, View view, FailReason failReason) {
+                    ivProgress.setVisibility(View.GONE);
 //                photoView.setImageResource(R.drawable.no_pic);
-            }
+                }
 
-            @Override
-            public void onLoadingComplete(String s, View view, Bitmap bitmap) {
-                //加载完成
-                ivProgress.setVisibility(View.GONE);
-                photoView.setImageBitmap(bitmap);
-                loadBitmap=bitmap;
-            }
+                @Override
+                public void onLoadingComplete(String s, View view, Bitmap bitmap) {
+                    //加载完成
+                    ivProgress.setVisibility(View.GONE);
+                    photoView.setImageBitmap(bitmap);
+                    loadBitmap=bitmap;
+                }
 
-            @Override
-            public void onLoadingCancelled(String s, View view) {
-                ivProgress.setVisibility(View.GONE);
+                @Override
+                public void onLoadingCancelled(String s, View view) {
+                    ivProgress.setVisibility(View.GONE);
 
-            }
-        });
+                }
+            });
+        }
+
     }
 
     /**
